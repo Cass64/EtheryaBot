5bb9892005b8180c30916805aa4beb9c256cfd25
Cass64 <nolhan.vandenbroeck@gmail.com>
2025-02-18 21:40:39 +0100

    Update start.py

--- start.py
+++ start.py
@@ -22,7 +22,7 @@ bot = commands.Bot(command_prefix="!!", intents=intents)
 async def on_ready():
     print(f"Bot connecté en tant que {bot.user}")
 
-#------------------------------------------------------------------------- Commandes de modération : addrole et removerole
+#------------------------------------------------------------------------- Commandes d'économie : !!break
 
 from discord.ext import commands
 
@@ -36,25 +36,25 @@ async def breakk(ctx, membre: discord.Member):
     """
 
     ROLE_REQUIRED = "″ [𝑺ץ] Perm Anti Protect"  # Rôle requis pour exécuter la commande
-    ROLE_TO_REMOVE_MALUS = "″ [𝑺ץ] Protect !!rob"       # Rôle à ajouter au membre ciblé
+    ROLE_TO_REMOVE_BREAK = "″ [𝑺ץ] Protect !!rob"       # Rôle à ajouter au membre ciblé
     ROLE_TO_REMOVE = "″ [𝑺ץ] Perm Anti Protect"     # Rôle à retirer à l'exécutant
 
     role_required = discord.utils.get(ctx.guild.roles, name=ROLE_REQUIRED)
-    role_to_remove_malus = discord.utils.get(ctx.guild.roles, name=ROLE_TO_REMOVE_MALUS)
+    role_to_remove_break = discord.utils.get(ctx.guild.roles, name=ROLE_TO_REMOVE_BREAK)
     role_to_remove = discord.utils.get(ctx.guild.roles, name=ROLE_TO_REMOVE)
 
-    if not role_required or not role_to_remove_malus or not role_to_remove:
+    if not role_required or not role_to_remove_break or not role_to_remove:
         return await ctx.send("❌ L'un des rôles spécifiés n'existe pas.")
 
     if role_required not in ctx.author.roles:
       return await ctx.send("❌ Vous n'avez pas la permission d'utiliser cette commande.")
 
 # Vérifie si le membre a le rôle avant de le retirer
-    if role_to_remove_malus not in membre.roles:
-        await ctx.send(f"{membre.mention} n'a pas le rôle {role_to_remove_malus.mention}. <:haram:1176229029796380702>")
+    if role_to_remove_break not in membre.roles:
+        await ctx.send(f"{membre.mention} n'a pas le rôle {role_to_remove_break.mention}. <:haram:1176229029796380702>")
     else:
-        await membre.remove_roles(role_to_remove_malus)
-        await ctx.send(f"Le rôle {role_to_remove_malus.mention} a été enlevé. <a:fete:1172810362261880873>")
+        await membre.remove_roles(role_to_remove_break)
+        await ctx.send(f"Le rôle {role_to_remove_break.mention} a été enlevé. <a:fete:1172810362261880873>")
 
 
     # Retirer le rôle à l'exécutant
@@ -64,6 +64,48 @@ async def breakk(ctx, membre: discord.Member):
     else:
         await ctx.send(f"{ctx.author.mention}, vous n'aviez pas le rôle {role_to_remove.mention}. ❌")
 
+#------------------------------------------------------------------------- Commandes d'économie : !!malus
+
+AUTHORIZED_ROLES = ["″ [𝑺ץ] Perm Ajout Malus"]
+
+@bot.command(name="malus")
+async def malus(ctx, membre: discord.Member):
+    """Ajoute un rôle fixe à un utilisateur, retire un autre rôle fixe à l'exécutant, 
+       et supprime le rôle ajouté après une durée spécifiée.
+       Seuls ceux ayant '[𝑺ץ] Perm Anti Protect' peuvent utiliser cette commande.
+    """
+
+    ROLE_REQUIRED_MALUS = "″ [𝑺ץ] Perm Ajout Malus"  # Rôle requis pour exécuter la commande
+    ROLE_TO_ADD_MALUS = "″ [𝑺ץ] Malus"  # Le rôle temporaire à ajouter
+    ROLE_TO_REMOVE_MALUS = "″ [𝑺ץ] Perm Ajout Malus"  # Rôle à retirer à l'exécutant
+
+    role_required = discord.utils.get(ctx.guild.roles, name=ROLE_REQUIRED_MALUS)
+    role_to_add = discord.utils.get(ctx.guild.roles, name=ROLE_TO_ADD_MALUS)
+    role_to_remove = discord.utils.get(ctx.guild.roles, name=ROLE_TO_REMOVE_MALUS)
+
+    if not role_required_malus or not role_to_add_malus or not role_to_remove_malus:
+        return await ctx.send("❌ L'un des rôles spécifiés n'existe pas.")
+
+    if role_required_malus not in ctx.author.roles:
+        return await ctx.send("❌ Vous n'avez pas la permission d'utiliser cette commande.")
+
+    # Ajouter le rôle temporaire à l'utilisateur
+    await membre.add_roles(role_to_add_malus)
+    await ctx.send(f"Le rôle {role_to_add_malus.mention} a été ajouté. <a:fete:1172810362261880873>") 
+
+    # Retirer le rôle à l'exécutant
+    if role_to_remove_malus in ctx.author.roles:
+        await ctx.author.remove_roles(role_to_remove_malus)
+        await ctx.send(f"Le rôle {role_to_remove_malus.mention} a été retiré. <a:fete:1172810362261880873>")
+    else:
+        await ctx.send(f"{ctx.author.mention}, vous n'aviez pas le rôle {role_to_remove_malus.mention}. <:haram:1176229029796380702>")
+
+    # Temps pendant lequel le rôle restera (exemple : 1 heure)
+    await asyncio.sleep(86400)  # 3600 secondes = 1 heure
+
+    # Retirer le rôle après le délai
+    await membre.remove_roles(role_to_add_malus)
+    await ctx.send(f"Le rôle {role_to_add_malus.mention} a été retiré de {membre.mention} après 1 jour. ⏳")
 
 #------------------------------------------------------------------------- Lancement du bot
 keep_alive()
