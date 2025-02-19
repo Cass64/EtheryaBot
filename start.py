0e53bbf95b783c44c8000427cf7b69e94a5e3b81
Cass64 <nolhan.vandenbroeck@gmail.com>
2025-02-19 22:19:38 +0100

    Update start.py

--- start.py
+++ start.py
@@ -328,7 +328,7 @@ async def on_message(message):
 
 
         embed.set_thumbnail(url="https://github.com/Cass64/EtheryaBot/blob/main/images_etherya/etheryBot_profil.jpg?raw=true")  # Remplace par l'URL de l'image en haut à droite
-        embed.set_footer(text="Utilise ces commandes avec sagesse !", icon_url="../images_etherya/etheryaBot_banniere.png")  # Remplace par l'URL de l'image en bas
+        embed.set_footer(text="Utilise ces commandes avec sagesse !", icon_url="https://github.com/Cass64/EtheryaBot/blob/main/images_etherya/etheryaBot_banniere.png?raw=true")  # Remplace par l'URL de l'image en bas
 
         await message.channel.send(embed=embed)
 
