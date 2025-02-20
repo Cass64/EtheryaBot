60cd717eb0f3ed7162d399f736e94bb165000ef3
Cass64 <nolhan.vandenbroeck@gmail.com>
2025-02-20 23:07:54 +0100

    Update start.py

--- start.py
+++ start.py
@@ -16,7 +16,7 @@ load_dotenv()
 # Connexion MongoDB
 mongo_uri = os.getenv("MONGO_URI")
 client = MongoClient(mongo_uri)
-db = client['Etherya2']
+db = client['Etherya3']
 collection = db['commande_eco']
 store_collection = db['store']
 
