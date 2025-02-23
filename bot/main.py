807db17674a89eb8f4d1af4063d465cc860e54f4
Cass64 <nolhan.vandenbroeck@gmail.com>
2025-02-23 02:21:52 +0100

    Add files via upload

---
+++ bot/main.py
@@ -0,0 +1,52 @@
+import os  
+from dotenv import load_dotenv
+from discord import app_commands
+import discord
+from discord.ext import commands
+from keep_alive import keep_alive
+import random
+import json
+import asyncio
+import pymongo
+from pymongo import MongoClient
+import datetime
+import math
+
+# Chargement des variables d'environnement
+load_dotenv()
+TOKEN = os.getenv('TOKEN_BOT_DISCORD')
+
+# Connexion MongoDB
+mongo_uri = os.getenv("MONGO_URI")
+client = MongoClient(mongo_uri)
+db = client['Cass-Eco2']
+
+# Vérification de la connexion MongoDB
+try:
+    client.admin.command('ping')
+    print("✅ Connexion à MongoDB réussie !")
+except Exception as e:
+    print(f"❌ Échec de connexion à MongoDB : {e}")
+    exit()
+
+# Intents et configuration du bot
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!!", intents=intents)
+bot.db = db  # Ajouter la base de données à l'objet bot
+
+# Événement lorsque le bot est prêt
+@bot.event
+async def on_ready():
+    print(f"Bot connecté en tant que {bot.user}")
+    # Chargement des cogs
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            bot.load_extension(f'cogs.{filename[:-3]}')
+            print(f'Cog {filename[:-3]} chargé.')
+    # Synchronisation des commandes slash
+    await bot.tree.sync()
+    print("Commandes slash synchronisées.")
+
+# Démarrage du bot
+keep_alive()
+bot.run(TOKEN)
