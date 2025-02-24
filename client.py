477df4826bda30929af43d9eda7e3b2ae1451cdd
Cass64 <nolhan.vandenbroeck@gmail.com>
2025-02-24 19:05:45 +0100

    Update client.py

--- client.py
+++ client.py
@@ -3,8 +3,8 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-TOKEN = os.getenv('TOKEN_BOT_DISCORD')
+token = os.getenv('TOKEN_BOT_DISCORD')
 
 client = discord.Client(intents=discord.Intents.all())
 
-client.run(TOKEN=token)
+client.run(token=token)
