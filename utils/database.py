49cc32d864a0d40866ef2b77fafcbeaa12e1cb1f
Cass64 <nolhan.vandenbroeck@gmail.com>
2025-04-27 15:56:18 +0200

    Update database.py

--- utils/database.py
+++ utils/database.py
@@ -2,9 +2,9 @@ from pymongo import MongoClient
 
 client = None
 
-def connect_to_mongo(uri):
+def connect_to_mongo(MONGO_URI):
     global client
-    client = MongoClient(uri)
+    client = MongoClient(MONGO_URI)
     print("✅ Connecté à MongoDB")
 
 def get_database(name):
